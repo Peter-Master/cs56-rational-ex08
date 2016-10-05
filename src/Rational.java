c9ab57da5f320f0112f0f8ffb4cf1f346091ab02
Peter Master <petehoke@comcast.net>
2016-10-04 18:04:43 -0700

    fix constructor that takes arguments to never have a negative denom; order indentation

--- src/Rational.java
+++ src/Rational.java
@@ -72,40 +72,46 @@ public class Rational {
     }
     
     public Rational() {
-	this.num = 1;
-	this.denom = 1;
+		this.num = 1;
+		this.denom = 1;
     }
 
     public Rational(int num, int denom) {
-	if (denom== 0) {
-	    throw new IllegalArgumentException("denominator may not be zero");
-	}
-	this.num = num;
-	this.denom = denom;
-	if (num != 0) {
-	    int gcd = Rational.gcd(num,denom);
-	    this.num /= gcd;
-	    this.denom /= gcd;
-	}
+		if (denom== 0) {
+	    	throw new IllegalArgumentException("denominator may not be zero");
+		}
+		if (this.denom < 0) {
+			this.num = -1*num;
+			this.denom = -1*denom;
+		}
+		else {
+			this.num = num;
+			this.denom = denom;
+		}
+		if (num != 0) {
+	    	int gcd = Rational.gcd(num,denom);
+	    	this.num /= gcd;
+	    	this.denom /= gcd;
+		}
     }
 
     public String toString() {
-	if (denom == 1 || num == 0)
-	    return "" + num;
-	return num + "/" + denom;
+		if (denom == 1 || num == 0)
+	    	return "" + num;
+		return num + "/" + denom;
     }
 
     public int getNumerator() { return this.num; }
     public int getDenominator() { return this.denom; }
 
     public Rational times(Rational r) {
-	return new Rational(this.num * r.num,
-			    this.denom * r.denom);
+		return new Rational(this.num * r.num,
+			    			this.denom * r.denom);
     }
 
     public static Rational product(Rational a, Rational b) {
 	return new Rational(a.num * b.num,
-			    a.denom * b.denom);
+			    		a.denom * b.denom);
     }
 
     
@@ -115,9 +121,9 @@ public class Rational {
      */
 
     public static void main (String [] args) {
-	Rational r = new Rational(5,7);
-	System.out.println("r.getNumerator()=" + r.getNumerator());
-	System.out.println("r.getDenominator()=" + r.getDenominator());
+		Rational r = new Rational(5,7);
+		System.out.println("r.getNumerator()=" + r.getNumerator());
+		System.out.println("r.getDenominator()=" + r.getDenominator());
     }
 
     
