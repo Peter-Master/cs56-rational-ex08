a716f210f6a211c6836c5d9514eab10bbbf41e28
Peter Master <petehoke@comcast.net>
2016-10-05 17:56:35 -0700

    reorder constructor and methods and add @param comments

--- src/Rational.java
+++ src/Rational.java
@@ -15,7 +15,31 @@ public class Rational {
     private int num;
     private int denom;
 
-    /**
+    public Rational() {
+		this.num = 1;
+		this.denom = 1;
+    }
+
+    public Rational(int num, int denom) {
+		if (denom== 0) {
+	    	throw new IllegalArgumentException("denominator may not be zero");
+		}
+		else if (denom < 0) {
+			this.num = -1*num;
+			this.denom = -1*denom;
+		}
+		else {
+			this.num = num;
+			this.denom = denom;
+		}
+		if (num != 0) {
+	    	int gcd = Rational.gcd(num,denom);
+	    	this.num /= gcd;
+	    	this.denom /= gcd;
+		}
+    }
+
+	/**
 	greatest common divisor of a and b
 	@param a first number
 	@param b second number
@@ -32,12 +56,27 @@ public class Rational {
 	    return gcd(b%a, a);
     }
 
+    /**
+    lowest common multiple of a and b
+    @param a first number
+    @param b second number
+    @return lcm of a and b
+    */
     public static int lcm(int a, int b) {
     	if (b == 0)
     		return 0;
     	return abs(a) / gcd(a, b) * abs(b);
     }
 
+	public Rational times(Rational r) {
+		return new Rational(this.num * r.num,
+			    			this.denom * r.denom);
+    }
+
+    public static Rational product(Rational a, Rational b) {
+	return new Rational(a.num * b.num,
+			    		a.denom * b.denom);
+    }
 
     public Rational plus(Rational r) {
     	return 	new Rational(this.num * r.denom +
@@ -72,30 +111,6 @@ public class Rational {
     public static Rational quotient(Rational a, Rational b) {
     	return a.times(b.reciprocalOf());
     }
-    
-    public Rational() {
-		this.num = 1;
-		this.denom = 1;
-    }
-
-    public Rational(int num, int denom) {
-		if (denom== 0) {
-	    	throw new IllegalArgumentException("denominator may not be zero");
-		}
-		else if (denom < 0) {
-			this.num = -1*num;
-			this.denom = -1*denom;
-		}
-		else {
-			this.num = num;
-			this.denom = denom;
-		}
-		if (num != 0) {
-	    	int gcd = Rational.gcd(num,denom);
-	    	this.num /= gcd;
-	    	this.denom /= gcd;
-		}
-    }
 
     public String toString() {
 		if (denom == 1 || num == 0)
@@ -105,17 +120,6 @@ public class Rational {
 
     public int getNumerator() { return this.num; }
     public int getDenominator() { return this.denom; }
-
-    public Rational times(Rational r) {
-		return new Rational(this.num * r.num,
-			    			this.denom * r.denom);
-    }
-
-    public static Rational product(Rational a, Rational b) {
-	return new Rational(a.num * b.num,
-			    		a.denom * b.denom);
-    }
-
     
     /** 
 	For testing getters.  
